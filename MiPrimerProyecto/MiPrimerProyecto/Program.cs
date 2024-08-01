939a2c2b5378c8e5016ce531481c498f8c9049a8
Jennifer <jennferbaires@gmail.com>
2024-07-31 21:14:38 -0600

    Tipos de datos C#

--- MiPrimerProyecto/MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -10,25 +10,27 @@ namespace MiPrimerProyecto
     {
         static void Main(string[] args)
         {
+
             Console.Write("Num1:");
-            int num1 = int.Parse(Console.ReadLine());
             sbyte num1 = sbyte.Parse(Console.ReadLine());
+            Boolean comprobar = true;
+            Console.WriteLine("El valor es: {0}", comprobar);
 
             Console.Write("Num2:");
-            int num2 = int.Parse(Console.ReadLine());
-            Console.Write("Num2:");
-            sbyte num2 = sbyte.Parse(Console.ReadLine());
+            sbyte Num2 = sbyte.Parse(Console.ReadLine());
+            Console.Write("Nombre:");
+            string nombre = Console.ReadLine();
 
-            int respuesta = num1 + num2;
-            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+            int suma = Num1 + Num2;
 
-            int suma = num1 + num2;
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, Num2, suma);
+            Console.WriteLine("Hola {0} bienvenido", nombre);
 
-            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, suma);
-
+            DateTime fechaActual = new DateTime();
+            fechaActual = DateTime.Now;
+            Console.WriteLine("La fecha actual es: {0}", fechaActual);
             //Pausa.
             Console.ReadLine();
-        }
-   
+
     }
 }
