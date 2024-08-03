f96878757712484c6c1388efef56ad96041cdc60
Jennifer <jennferbaires@gmail.com>
2024-08-02 20:36:52 -0600

    Determinar el promedio de una serie de numeros

--- MiPrimerProyecto/MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -41,9 +41,18 @@ namespace MiPrimerProyecto
 
             double c2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
             Console.WriteLine("La nota de C2 es: {0}", c2);
-            
+
             //TAREA: Obtener la Nota Final por computo y Nota Final de la materia de Programacion I.
-            
+
+
+            //ejercicio obtener el promedio de una serie de numeros.
+            int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
+            int suma = 0;
+            foreach (int num in serie){
+                suma += num;
+            }
+            decimal prom = suma / serie.Length;
+            Console.WriteLine("La suma es: {0}, el promedio {1}", suma, prom);
             //Pausa.
             Console.ReadLine();
         }
