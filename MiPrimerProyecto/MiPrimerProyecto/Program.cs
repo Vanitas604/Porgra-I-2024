ef6850423e8759419e05b25d629a6d75ed44fb08
Jennifer <jennferbaires@gmail.com>
2024-08-02 20:10:08 -0600

    Nota final de Computo 1 y 2 + tarea

--- MiPrimerProyecto/MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -31,6 +31,21 @@ namespace MiPrimerProyecto
 
             double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
             Console.WriteLine("La nota de C1 es: {0}", c1);
+
+            Console.Write("Lab1:");
+            lab1 = double.Parse(Console.ReadLine()); //8
+
+            Console.Write("Lab2:");
+            lab2 = double.Parse(Console.ReadLine()); //9
+
+            Console.Write("Parcial 1:");
+            parcial1 = double.Parse(Console.ReadLine()); //7
+
+            double c2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+            Console.WriteLine("La nota de C2 es: {0}", c2);
+            
+            //TAREA: Obtener la Nota Final por computo y Nota Final de la materia de Programacion I.
+            
             //Pausa.
             Console.ReadLine();
         }
