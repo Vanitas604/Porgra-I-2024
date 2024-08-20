3b227bf19d7df8adbfc585b8e9d11e708897c66c
Jennifer <jennferbaires@gmail.com>
2024-08-20 16:57:47 -0600

    Serie de numeros introducidos por el usuario separados por comas, para obtener la media y la desviacion tipica

--- MiPrimerProyecto/MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -8,43 +8,41 @@ namespace MiPrimerProyecto {
     class Program {
         static void Main(string[] args) {
             //Uso de funciones...
-            Console.WriteLine("La suma de 10+5={0}", suma());
-            //ejercicio encontrar la media aritmetica y la desviacion tipica de una seria de numeros.
+            //ejercicio encontrar la media aritmetica y la desviación típica de una serie de números.
 
-            Console.Write("Num 1: ");
-            int num1 = int.Parse(Console.ReadLine());
-
-            Console.Write("Num 2: ");
-            int num2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("La suma de {0}+{1}={2}", num1, num2, suma(num1, num2));
             int[] serie = new int[] { 1, 2, 3, 4, 5 };
             Console.WriteLine("La media aritmetica es: {0}, y la desviacion tipica es: {1}", media(serie), tipica(serie));
+            Console.Write("Ingrese la serie de numeros separados por comas: ");
+            String serie = Console.ReadLine();
+            String[] numeros = serie.Split(',');
+
+            Console.WriteLine("La media aritmetica es: {0}, y la desviacion tipica es: {1}", media(numeros), tipica(numeros));
 
             Console.ReadLine();
         }
-        static int suma() {
-            int respuesta = 10 + 5;
-            return respuesta;
-            static double media(int[] serie) {
+        static double media(int[] serie) {
+            static double media(string[] serie) {
                 double media = 0;
                 foreach (int num in serie) {
                     media += num;
-                }
-                return media / serie.Length;
-            }
-
-            static int suma(int a, int) {
-                int respuesta = a + b;
-                return respuesta;
-                static double tipica(int[] serie) {
-                    double tipica = 0,
-                            m = media(serie);
-                    foreach (int num in serie) {
-                        tipica += Math.Pow(num - m, 2);
+                    foreach (string num in serie) {
+                        media += int.Parse(num);
                     }
-                    tipica = Math.Sqrt(tipica / serie.Length);
-                    return Math.Round(tipica, 2);
+                    return media / serie.Length;
+                }
+                static double tipica(int[] serie) {
+                    static double tipica(string[] serie) {
+                        double tipica = 0,
+                                m = media(serie);
+                        foreach (int num in serie) {
+                            tipica += Math.Pow(num - m, 2);
+                            foreach (string num in serie) {
+                                tipica += Math.Pow(int.Parse(num) - m, 2);
+                            }
+                            tipica = Math.Sqrt(tipica / serie.Length);
+                            return Math.Round(tipica, 2);
+                        }
+                    }
                 }
             }
         }
